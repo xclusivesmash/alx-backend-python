f3917ea651ebd85d529b8d01a28f3efa6f3bea79
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-22 10:03:37 +0000

    Initial commit

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+module: 3-to_str
+description: returns str. rep. of an object.
+"""
+
+
+def to_str(n: float) -> str:
+    """Type annotations.
+    Args:
+        n (float): input.
+    Return:
+        (str): cast of n to str.
+    """
+    return str(n)
