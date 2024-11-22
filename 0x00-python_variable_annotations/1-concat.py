28b783490883c5350e2cdcf190325fa7f3a21231
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-22 09:03:03 +0000

    Initial commit

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+module: 1-concat
+Description: concatenation of 2 strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Type annotations.
+    Args:
+        str1 (str): input one.
+        str2 (str): input two.
+    Return:
+        combo (str): str1 + str2
+    """
+    return str1 + str2
