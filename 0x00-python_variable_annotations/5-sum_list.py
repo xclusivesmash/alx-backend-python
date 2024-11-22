33513d8712bf1fcea2924b4996739ce5a0ef48e9
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-22 10:12:22 +0000

    Initial commit

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+module: 5-sum_list
+description: sums list items.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Type annotations.
+    Args:
+        input_list (list[float]): list of float items.
+    Return:
+        (float): sum.
+    """
+    return sum(input_list)
