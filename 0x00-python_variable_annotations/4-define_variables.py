e14eea2fad914e4c48840b6e1b2925c0d783f4db
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-22 10:07:37 +0000

    Initial commit

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+module: 4-define_variables
+description: more on type annotations.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
