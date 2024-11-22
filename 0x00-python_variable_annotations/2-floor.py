cd54eecc43c2be2c61ac7e0d5da2ac3fbf947c95
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-22 09:11:52 +0000

    Initial commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+module: 2-floor
+Description: computes the floor of a float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Type annotations.
+    Args:
+        n (float): input argument.
+    Return:
+        (int): floor(n).
+    """
+    return math.floor(n)
