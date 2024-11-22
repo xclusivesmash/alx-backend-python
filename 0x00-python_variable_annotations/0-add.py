d9d83f605f45d082729380cb6bce7c1aff0e8ff1
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-22 08:58:03 +0000

    Initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module: 0-add
+Description: typed-annotated add function.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Type-annotations.
+    Args:
+        a (float): first argument.
+        b (float): second argument.
+    Return:
+        sum (float): a + b.
+    """
+    return a + b
