e23e677a94597409c876252800b654c0f4ac1880
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-26 15:56:20 +0000

    Updated code

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """yield integers.
     Return:
         list of number btw. 0 and 10.
